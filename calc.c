e1408645658d501203379f35dc2c09700f21ea85
LiAlexander <alexgrandbell@gmail.com>
2019-11-01 11:26:59 -0400

    Changed back to if/else, added a while loop

--- calc.c
+++ calc.c
@@ -8,48 +8,53 @@ double multiply(double a, double b);
 double divide(double a, double b);
 
 
-int main(void)
+
+int main()
 {
     double a = 0;
     double b = 0;
     char op;
+    int on = 1;
 
-
-
-    printf("Choose an operator: ");
-    scanf("%c", &op);
-
-    // get numbers
     printf("Choose a number: ");
     scanf("%lf", &a);
 
-
-
-    printf("Choose a number: ");
-    scanf("%lf", &b);
-
-
-    printf("%f %c %f = ", a,op, b);
-
-     switch(op)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",a, b, add(a,b));
-            break;
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",a, b, subtract(a,b));
-            break;
-        case '*':
-            printf("%.1lf * %.1lf = %.1lf",a, b, multiply(a,b));
-            break;
-        case '/':
-            printf("%.1lf / %.1lf = %.1lf",a, b, divide(a,b));
-            break;
-        // operator doesn't match any case constant (+, -, *, /)
-        default:
-            printf("Error! Operator is not correct");
-    }
     
+
+    
+
+
+    //printf("%f %c %f = ", a,op, b);
+  do{
+        printf("Choose an operator: ");
+        scanf(" %c", &op);
+        printf("Choose a number: ");
+        
+        scanf("%lf", &b);
+        
+        if(op == '+'){
+            printf("%.1lf + %.1lf = %.1lf",a, b, add(a,b));
+            a = (add(a,b));
+            
+            }
+        else if(op == '-'){
+            printf("%.1lf - %.1lf = %.1lf",a, b, subtract(a,b));
+            a = subtract(a,b);
+            }
+        else if(op == '*'){
+            printf("%.1lf * %.1lf = %.1lf",a, b, multiply(a,b));
+            a = multiply(a,b);
+        }
+        else if(op == '/'){
+            printf("%.1lf / %.1lf = %.1lf",a, b, divide(a,b));
+            a= divide(a,b);
+            }
+      
+        
+      printf("\nType 0 to leave, 1 to stay");
+      scanf("%d", &on);
+    
+    }while(on == 1);
     return 0;
 }
     
