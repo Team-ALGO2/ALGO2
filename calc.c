bb599215e3d0b584637aae84b1f8e18e73f719e8
LiAlexander <alexgrandbell@gmail.com>
2019-11-02 11:49:14 -0400

    Added sine and cosine. The math also works continually like in a normal calculator.

--- calc.c
+++ calc.c
@@ -1,59 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
 double divide(double a, double b);
 
-
+#define PI 3.141592653589
 
 int main()
 {
     double a = 0;
     double b = 0;
-    char op;
+    char op[3];
     int on = 1;
 
-    printf("Choose a number: ");
-    scanf("%lf", &a);
-
+    //printf("Choose a number: ");
+    //printf("0");
     
-
+    system("clear");
+    scanf("%lf", &a);
     
 
 
     //printf("%f %c %f = ", a,op, b);
   do{
-        printf("Choose an operator: ");
-        scanf(" %c", &op);
-        printf("Choose a number: ");
+      
         
+        
+        scanf("%s",op);
         scanf("%lf", &b);
         
-        if(op == '+'){
-            printf("%.1lf + %.1lf = %.1lf",a, b, add(a,b));
+        if(strcmp(op, "+") == 0){
+            printf("%.9lf",add(a,b));
             a = (add(a,b));
             
             }
-        else if(op == '-'){
-            printf("%.1lf - %.1lf = %.1lf",a, b, subtract(a,b));
+        else if(strcmp(op, "-") == 0){
+            printf("%.9lf", subtract(a,b));
             a = subtract(a,b);
             }
-        else if(op == '*'){
-            printf("%.1lf * %.1lf = %.1lf",a, b, multiply(a,b));
+        else if(strcmp(op, "*") == 0){
+            printf("%.9lf", multiply(a,b));
             a = multiply(a,b);
         }
-        else if(op == '/'){
-            printf("%.1lf / %.1lf = %.1lf",a, b, divide(a,b));
+        else if(strcmp(op, "/") == 0){
+            printf("%.9lf", divide(a,b));
             a= divide(a,b);
             }
-      
+        else if(strcmp(op, "sin") == 0){
+        printf("%.9lf", sin(b*PI/180));
+        a= sin(b*PI/180);
+        }
+        else if(strcmp(op, "cos") == 0){
+        printf("%.9lf", cos(b*PI/180));
+        a= sin(b*PI/180);
+        }
+
         
-      printf("\nType 0 to leave, 1 to stay");
-      scanf("%d", &on);
-    
+  
+    system("clear");
     }while(on == 1);
     return 0;
 }
