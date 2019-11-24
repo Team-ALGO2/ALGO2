a9acdbae2811fbedfb43a1315d23d6efe7296476
tommcn <tommcn@mcnamer.ca>
2019-11-24 09:58:02 -0500

    Update main.c

--- Maps/main.c
+++ Maps/main.c
@@ -13,6 +13,7 @@ typedef struct{
 } vertice;
 
 
+
 int main(void)
 {
 
@@ -32,43 +33,45 @@ int main(void)
     vertice ALL_vertice[] = {one, two, three, four, five};
     int all_length = 5;
 
+    node ALL_node[] = {A, B, C, D};
+    int node_length = 4;
+
+    
+
     node s = A;
-
-    for (int i = 1; i < all_length; i++)
+    // for all the nodes do
+    for (int i = 0; i < node_length; i++)
     {
+        s = ALL_node[i];
 
-        // iterate over vertices 
+        // iterate over all vertices 
         for (int j = 0; j < all_length; j++)
         {
-            // find which ones leave from s
-            if (ALL_vertice[j].from.name == s.name)
+            node cur = ALL_vertice[j].from;
+            // find which ones leave from cur
+            if (cur.name == s.name)
             {
                 // if the weigth is smaller than the current disctance
-                if (ALL_vertice[j].too.distance_to > ALL_vertice[j].weigth + s.distance_to)
+                if (ALL_vertice[j].too.distance_to > ALL_vertice[j].weigth + ALL_vertice[j].from.distance_to)
                 {
-                    printf("\n%d\n", ALL_vertice[j].too.distance_to); //intial
 
-                    ALL_vertice[j].too.distance_to = ALL_vertice[j].weigth;
+                    printf("From: %d -- To: %d: ", ALL_vertice[j].from.name, ALL_vertice[j].too.name);
+                    printf("ALL_vertice[j].from.distance_to = %d, ALL_vertice[j].from = %d\n", ALL_vertice[j].from.distance_to, ALL_vertice[j].from.name);
+                    printf("%d --> ",ALL_vertice[j].too.distance_to);
+                    
+                    ALL_vertice[j].too.distance_to = ALL_vertice[j].weigth + ALL_vertice[j].from.distance_to;
+                    printf("%d \n",ALL_vertice[j].too.distance_to);
+                    printf("==================\n");
 
-                    printf("%d\n", ALL_vertice[j].too.distance_to); // after
                 }
             }
         }
 
-        printf("==================\n");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~\n");
 
 
-
-
-    
 
     }
-
-
-
-
-
-
 
 
 }
