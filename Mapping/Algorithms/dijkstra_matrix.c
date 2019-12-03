4eb38665abb20b4202ffefac3246fe60a21e1e9a
LiAlexander <alexgrandbell@gmail.com>
2019-12-02 19:51:35 -0500

    Update dijkstra_matrix.c

---
+++ Mapping/Algorithms/dijkstra_matrix.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+#define max 100
+
+typedef struct e{
+    int u,v,w;
+}e;
+
+typedef struct elist{
+    e data[max];
+    int n;
+}elist;
+
+int D[max][max],n;
+
+
+
+
+int main(){
+
+    int i,j,total_cost, n;
+	
+	printf("\nEnter number of vertices:");
+	
+	scanf("%d",&n);
+	
+	printf("\nAdjacency matrix:\n");
+	
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&D[i][j]);
+		}
+	}	
+    //int cost;
+
+    printf("======================\n");
+    for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			printf("%d ",D[i][j]);
+		}
+        printf("\n");
+	}	
+
+
+}
