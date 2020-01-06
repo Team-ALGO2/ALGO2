ff3263ed75f2bce50710fd9ee57fb474086f6153
Edward Li <radioactivehydra@gmail.com>
2020-01-05 20:10:35 -0500

    Fixed Dijkstras Max Naming

--- Mapping/Algorithms/dijkstra_matrix.c
+++ Mapping/Algorithms/dijkstra_matrix.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
-#define max 9
+#define MAX_SIZE 9
 
-void debugPrint(int dist[max]){
+void debugPrint(int dist[MAX_SIZE]){
 	printf("Vertex tDistance from Source\n");
-	for(int node = 0; node < max; node++){
+	for(int node = 0; node < MAX_SIZE; node++){
 		printf("%d t %d\n", node, dist[node]);
 	}
 }
 
-int shortestDist(int dist[max], int checked[max]){
+int shortestDist(int dist[MAX_SIZE], int checked[MAX_SIZE]){
 	int min = INFINITY;
 	int minIndex;
-	for(int i = 0; i < max; i++){
+	for(int i = 0; i < MAX_SIZE; i++){
 		if(dist[i] < min && checked[i] == False){
 			min = dist[i];
 			minIndex = i;
@@ -21,19 +21,19 @@ int shortestDist(int dist[max], int checked[max]){
 	return minIndex;
 }
 
-int dijkstra(int map[max][max], int cMap[max][max], int start){
-	int dist[max];
-	for(int i = 0; i < max; i++){dist[i] = INFINITY;}
+int dijkstra(int map[MAX_SIZE][MAX_SIZE], int cMap[MAX_SIZE][MAX_SIZE], int start){
+	int dist[MAX_SIZE];
+	for(int i = 0; i < MAX_SIZE; i++){dist[i] = INFINITY;}
 
-	int checked[max];
-	for(int i = 0; i < max; i++){checked[i] = false;}
+	int checked[MAX_SIZE];
+	for(int i = 0; i < MAX_SIZE; i++){checked[i] = false;}
 
 	dist[start] = 0;
 
-	for(int cout = 0; cout < max; cout++){
+	for(int cout = 0; cout < MAX_SIZE; cout++){
 		int from = shortestDist(dist, checked);
 		checked[from] = true;
-		for(int to = 0; to < max; to++){
+		for(int to = 0; to < MAX_SIZE; to++){
 			//printf("%d\n", dist[to] > dist[from] + map[from][to]);
 			if(cMap[from][to] == true && dist[to] > dist[from] + map[from][to] && checked[to] == false){
 				dist[to] = dist[from] + map[from][to];
@@ -45,7 +45,7 @@ int dijkstra(int map[max][max], int cMap[max][max], int start){
 
 int main(){
 
-	int map[max][max] = {
+	int map[MAX_SIZE][MAX_SIZE] = {
 	{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
 	{4, 0, 8, 0, 0, 0, 0, 11, 0}, 
 	{0, 8, 0, 7, 0, 4, 0, 0, 2}, 
@@ -56,7 +56,7 @@ int main(){
 	{8, 11, 0, 0, 0, 0, 1, 0, 7}, 
 	{0, 0, 2, 0, 0, 0, 6, 7, 0}};
 
-	int cMap[max][max] = {
+	int cMap[MAX_SIZE][MAX_SIZE] = {
 	{0, 1, 0, 0, 0, 0, 0, 1, 0}, 
 	{1, 0, 1, 0, 0, 0, 0, 1, 0}, 
 	{0, 1, 0, 1, 0, 1, 0, 0, 1}, 
