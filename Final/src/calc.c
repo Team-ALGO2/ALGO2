6458ded16b008cc7a7689bca8f1e402871a5bafc
tommcn <42918822+tommcn@users.noreply.github.com>
2020-01-13 21:36:12 -0500

    Working on calc
    Co-Authored-By: SuperZooper3 <superzooper3@users.noreply.github.com>

--- Final/src/calc.c
+++ Final/src/calc.c
@@ -7,6 +7,7 @@ queue parseString(char* inputString)
     queue numberQ = {-1, -1, {0}};
     queue oppQ = {-1, -1, {0}};
     int len = strlen(inputString);
+    int isOpp = false;
 
     char numberC[10][2] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     char oppC[18][7] = {"+", "-", "*", "/", "%", "d%", "^", "sqrt", "!", "sin", "cos", "tan", "sineh", "cosineh", "aCb", "aPb", "(", ")"};
@@ -15,14 +16,23 @@ queue parseString(char* inputString)
     {
        if (isNum(inputString[i]))
        {
+           if (isOpp)
+           {
+               isOpp = false;
+               printf("%s was detected as an opperand (ID = %d) \n", opp, mathID(opp));
+                queue_enqueue(&oppQ, mathID(opp));
+           }
            printf("%c was detected as an int\n", inputString[i]);
            queue_enqueue(&numberQ, inputString[i]); //if i am correct we dont need atoi because inputing a char as an int will automaticaly convert
        } else {
            // go forward until next number to get full operator
            //get the string of the opperator
+           if (!isOpp)
+           {
+               
+           }
            char* opp = ")";
-           printf("%s was detected as an opperand (ID = %d) \n", opp, mathID(opp));
-           queue_enqueue(&oppQ, mathID(opp));
+           
        }
     }
 }
