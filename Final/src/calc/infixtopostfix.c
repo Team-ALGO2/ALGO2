f15c24da1d62e9c58939dab6f8b7e24109ec174e
SuperZooper3 <superzooper3@gmail.com>
2020-02-18 17:33:30 -0500

    Finished infixpostifx???

--- Final/src/calc/infixtopostfix.c
+++ Final/src/calc/infixtopostfix.c
@@ -19,7 +19,7 @@ int infixToPostfix(queue input, queue inputBits, queue * dataOut, queue * bitsOu
         // if it is an opperand then push to the output
         if(queue_getFront(&inputBits) == 0)
         {
-            printf("Pushing number: %d\n", stack_top(&working));
+            printf("Pushing number: %d\n", queue_getFront(&input));
             queue_enqueue(dataOut ,(queue_getFront(&input)));
             queue_dequeue(&input); //get rid of the data at the top of the queue
             queue_enqueue(bitsOut, 0); // used to identify if a number or char (0 is number)
@@ -28,7 +28,7 @@ int infixToPostfix(queue input, queue inputBits, queue * dataOut, queue * bitsOu
         // if it is a opperator (but not a '(' or a ')') then figure out if to push or directly to output
         else if (queue_getFront(&inputBits) == 1 && queue_getFront(&input) != 16 && queue_getFront(&input) != 17)
         {
-            printf("Pushing operator: %d\n", queue_getFront(&working));
+            printf("Pushing operator: %d\n", queue_getFront(&input));
             while (!stack_isEmpty(&working) && oppPres(stack_top(&working), queue_getFront(&input)) == -1 && stack_top(&working) != 16);
             {
                 queue_enqueue(dataOut, stack_top(&working)); //push the data
@@ -59,11 +59,11 @@ int infixToPostfix(queue input, queue inputBits, queue * dataOut, queue * bitsOu
     //need to push the remaining chars to the end once all of the opperands have been pushed
     while(!stack_isEmpty(&working))
     {
-        stack_push(dataOut, stack_top(&working)); //push the data
-        stack_push(bitsOut, 1); //push 1 because it is a char
+        queue_enqueue(dataOut, stack_top(&working)); //push the data
+        queue_enqueue(bitsOut, 1); //push 1 because it is a char
         stack_pop(&working);
     }
-    stack_printer(dataOut);
+    queue_printer(dataOut);
     printf("Done infix to postfix \n");
     return 0; //return 0 if done and not failed
 }
