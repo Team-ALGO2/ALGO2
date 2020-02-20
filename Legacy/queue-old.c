57e8182d3560d0b99c48b412eb320e2306fc325b
Edward Li <radioactivehydra@gmail.com>
2020-02-20 13:09:00 -0500

    Started Working On Stack and Queue Rewrite

---
+++ Legacy/queue-old.c
@@ -0,0 +1,293 @@
+// Queue is zero indexed
+
+/*
+ * Queue only accepts numbers
+ * Queue is initialized to -1 to help debugging
+ * 
+ * queue_getFront() returs the "top" (Last In Last Out)
+ * it will NOT pop it
+ * it will return false if queue is empty
+ * 
+ * queue_getRear() return the last value of queue. This value should stay the same until the queu is empty or until you add new values
+ * it will NOT pop it
+ * it will return false if queue is empty
+ * 
+ * queue_enqueue() inserts an int into the queue, at the end
+ * returns false if queue is full
+ * returns the value inserted if succesful
+ * 
+ * queue_dequeue() will remove the first value in the queue
+ * returns true if sucessful and false if not
+ * pop does NOT return the top value
+ * 
+ * 
+ * == UPDATE WITH THE USE OF STRUCTURES ==
+ * NOTE: QUEUE IS NOW A POINTER!!! 
+ * 
+ * 
+ * How To Create A Queue ->
+ * | queue queueName = {-1, -1, {0}};  |
+ *  Dont change the values unless you know what your doing
+ *  Note the 2 -1 values
+ * 
+ * How To Use Functions ->
+ * | function(&queueName, arguments)   |
+ * *Must Add `& Infront Of Stack Name`
+ * 
+ * Examples ->
+ * | queue test = {-1, -1, 0};;                 |
+ * | queue_enqueue(&test, 5)                 |
+ * | queue_getFront(&test)                   |
+ * | queue_dequeue(&test)                    |
+ * | queue_printer(&test)                    |
+ * 
+ * WARNING: We havent actually been taught (Pointers and Structs) yet, but oh well
+ * ¯\_(ツ)_/¯
+ * 
+*/
+
+//includes main utils
+#include "../main.h"
+
+
+
+//Create structure containing the queue and the traking variables
+typedef struct{
+    int front;
+    int rear;
+    int A[QUEUE_MAX_LENGTH];
+} queue;
+
+//a function to check if the stack is empty which may result in undefined behaviour
+int queue_isEmpty(queue *q)
+{
+    if (q->front == -1 && q->rear == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+//a funtion to check if the stack is full
+int queue_isFull(queue *q)
+{
+    //looks if the stack is at its maximum capacity
+    if ((q->rear + 1) % QUEUE_MAX_LENGTH == q->front)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+//Getting the current length of the queue
+int queue_length(queue *q)
+{   
+    if(!queue_isEmpty(q)){
+        return q->rear - q->front + 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+//adds to the back of the queue the value inputed
+int queue_enqueue(queue *q, int input)
+{
+    if(queue_isFull(q))
+    {   
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Queue is full, canot insert %d.\n", input);
+        #endif
+        return QUEUE_FULL;
+    }
+    else if (queue_isEmpty(q))
+    {
+        q->front = 0;
+        q->rear = 0;
+        q->A[q->rear] = input;
+        return true;
+    }
+    else
+    {
+        q->rear = (q->rear + 1) % QUEUE_MAX_LENGTH;
+        q->A[q->rear] = input;
+        return true;
+    }
+    
+    
+}
+
+//deques the front value
+int queue_dequeue(queue *q)
+{
+    if (queue_isEmpty(q))
+    {
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Queue is empty, canot dequeue.\n");
+        #endif
+        return QUEUE_EMPTY;
+    }
+    else if (q->front == q->rear)
+    {
+        q->front = -1;
+        q->rear = -1;
+        return true;
+    }
+    else
+    {
+        q->front = (q->front + 1) % QUEUE_MAX_LENGTH;
+        return true;
+    }
+}
+
+//a function to get the front value of the stack
+int queue_getFront(queue *q)
+{
+    //checks if the stack is empty and warns in that case
+    if (queue_isEmpty(q))
+    {
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Queue is empty, canot get the front value.\n Note: The value returned my be junk.\n");
+        #endif
+        return QUEUE_EMPTY;
+        
+    }
+    
+    return(q->A[q->front]);
+}
+
+//a function to get the rear value of the queue
+int queue_getRear(queue *q)
+{
+    //checks if the stack is empty and warns in that case
+    if (queue_isEmpty(q))
+    {
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Queue is empty, canot get the rear value.\n Note: The value returned my be junk.\n");
+        #endif
+        return QUEUE_EMPTY;
+    }
+    
+    return(q->A[q->rear]);
+}
+
+//prints out queue array WARN: THIS DOES NOT TAKE IN COUNT FRONT AND REAR. IT ONLY PRINTS OUT RAW ARRAY
+void queue_raw_printer(queue *q)
+{
+    printf("\n");
+    for (int i = 0; i < QUEUE_MAX_LENGTH; i++)
+    {
+        if(i == q->front){printf("[");}
+        printf("%i ", q->A[i]);
+        if(i == q->rear){printf("]");}
+    }
+    printf("\n");
+}
+
+void queue_printer(queue *q)
+{
+    printf("\n");
+    for (int i = 0; i < QUEUE_MAX_LENGTH; i++)
+    {
+        printf("%i ", q->A[(i + q->front) % QUEUE_MAX_LENGTH]);
+    }
+    printf("\n");
+}
+
+//Removed extra zeros
+void queue_printer_formatted(queue *q)
+{
+    printf("\n");
+    for (int i = 0; i < queue_length(q); i++)
+    {
+        printf("%i ", q->A[(i + q->front) % QUEUE_MAX_LENGTH]);
+    }
+    printf("\n");
+}
+
+
+/*
+ * 
+ * DATASTRUCTURE OF QUEUE (For Refrence)
+ * F - Front | R - Rear
+ * 
+ * -----------------
+ * 00000000 (Rest Cut Off To Save Space)
+ * ^
+ * |
+ * FR
+ * -----------------
+ * Current Values: []
+ * 
+ * queue_enqueue(&queue, 4)            |
+ * queue_enqueue(&queue, 5)            |
+ * queue_enqueue(&queue, 6)            |
+ * queue_enqueue(&queue, 7)            |
+ * queue_enqueue(&queue, 8)            |
+ * queue_enqueue(&queue, 9)            |
+ * -----------------
+ * 45678900
+ * ^    ^
+ * |    |
+ * F    R
+ * -----------------
+ * Current Values: [4, 5, 6, 7, 8, 9]
+ * 
+ * queue_dequeue(&queue)               |
+ * queue_dequeue(&queue)               |
+ * queue_dequeue(&queue)               |
+ * -----------------
+ * 45678900
+ *    ^ ^
+ *    | |
+ *    F R
+ * -----------------
+ * Current Values: [7, 8, 9]
+ * 
+ * Front Just Moves Up!
+ * 
+ * NEW: Circular array!
+ * 
+ * Front Can Loop To Back When Full, Not Surpassing Rear!!
+ * 
+ * queue_enqueue(&queue, 1)            |
+ * queue_enqueue(&queue, 1)            |
+ * queue_enqueue(&queue, 1)            |
+ * queue_enqueue(&queue, 1)            |
+ * -----------------
+ * 11678911
+ *  ^ ^ 
+ *  | | 
+ *  R F 
+ * -----------------
+ * Current Values: [7, 8, 9, 1, 1, 1, 1]
+ * 
+ * Tail Or Front Can Loop Back As well!
+ * 
+ * queue_dequeue(&queue)               |
+ * queue_dequeue(&queue)               |
+ * queue_dequeue(&queue)               |
+ * queue_dequeue(&queue)               |
+ * queue_dequeue(&queue)               |
+ * -----------------
+ * 11678911
+ * ^^  
+ * || 
+ * FR
+ * -----------------
+ * Current Values: [1, 1]
+ * 
+*/
+
---
+++ Legacy/stack-old.c
@@ -0,0 +1,179 @@
+// Stack is zero indexed
+
+/*
+ * Stack only accepts numbers
+ * Stack is initialized to -1 to help debugging
+ * 
+ * stack_top() return the top value of stack
+ * it will NOT pop it
+ * it will return false if stack is empty
+ * 
+ * stack_push() inserts an int into the stack and increase counter
+ * returns false if stack is full
+ * returns the value inserted if succesful
+ * 
+ * stack_pop() will decrese the counter by 1
+ * returns true if sucessful and false if not
+ * pop does NOT return the top value
+ * 
+ * 
+ * == UPDATE WITH THE USE OF STRUCTURES ==
+ * NOTE: STACK IS NOW A POINTER!!! 
+ * 
+ * 
+ * How To Create A Stack ->
+ * | stack stackName = {-1, {0}};      |
+ * *Dont change the values unless you know what your doing
+ * 
+ * How To Use Functions ->
+ * | function(&stackName, arguments)   |
+ * *Must Add `& Infront Of Stack Name`
+ * 
+ * Examples ->
+ * | stack test = {-1, {0}};                 | 
+ * | stack_push(&test, 5)                    |
+ * | stack_top(&test)                        |
+ * | stack_pop(&test)                        |
+ * | stack_printer(&test)                    |
+ * 
+ * WARNING: We havent actually been taught (Pointers and Structs) yet, but oh well
+ * ¯\_(ツ)_/¯
+ * 
+*/
+
+//includes main utils
+#include "../main.h"
+
+//Custom Stack Structire
+//counter -> The "pointer" of the top of the graph
+//A -> The Array where the stack elements will be stored
+typedef struct{
+    int counter;
+    int A[STACK_MAX_LENGTH];
+} stack;
+
+//Check if the stack is empty by checking the counter
+int stack_isEmpty(stack *s)
+{
+    if (s->counter < 0)
+    {
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+    
+}
+
+//Check if the stack is full by checking if the counter is one under the Max Length (STACK_MAX_LENGTH)
+int stack_isFull(stack *s)
+{   
+    if (s->counter + 1 >= STACK_MAX_LENGTH)
+    {
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+}
+
+//Getting the current length of the stack
+int stack_length(stack *s)
+{   
+    return s->counter;
+}
+
+//Getting the top element of stack
+int stack_top(stack *s)
+{
+    if (!stack_isEmpty(s))
+    {
+        return s->A[s->counter];
+    }
+    else
+    {
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Stack is empty, canot get the top value.\n Note: The value returned my be junk.\n");
+        #endif
+
+        return STACK_EMPTY;
+    }
+    
+}
+
+//Pushing element to the top of the stack
+int stack_push(stack *s, int x)
+{
+    if (!stack_isFull(s))
+    {
+        s->counter++;
+        s->A[s->counter] = x;
+        return s->A[s->counter];
+    }
+    else
+    {
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Stack is full, canot push.\n");
+        #endif
+        return STACK_FULL;
+    }
+    
+}
+
+//Removing element from the top of the stack
+int stack_pop(stack *s)
+{
+    if (!stack_isEmpty(s))
+    {
+        s->counter--;
+        return true;
+    }
+    else
+    {
+        //Error Message
+        #ifdef DEBUG
+        printf("Error: Stack is empty, canot pop.\n");
+        #endif
+        return STACK_EMPTY;
+    }
+    
+}
+
+//Debug printer for printing all elements of the stack
+void stack_printer(stack *s)
+{
+    printf("\n");
+    if(s->counter != -1){printf("[");}
+    for (int i = 0; i < STACK_MAX_LENGTH; i++)
+    {
+        printf("%i ", s->A[i]);
+        if(i == s->counter){printf("]");}
+    }
+    printf("\n");
+}
+
+//stack_printer without trailing zeros
+void stack_printer_formatted(stack *s){
+    for (int i = 0; i <= s->counter; i++)
+    {
+        printf("%i ", s->A[i]);
+    }
+    printf("\n");
+}
+
+//We dont need this >:(
+/*
+stack stack_reverse(stack *s)
+{
+    stack out = {-1, {0}};
+    for (int i = 0; i < stack_length(s); i++)
+    {
+        stack_push(&out, stack_pop(s));
+    }
+    return out;
+}
+*/
