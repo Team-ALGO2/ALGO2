47fc2510e042dc958c15eac76c39a57574d6fabd
Edward Li <radioactivehydra@gmail.com>
2020-02-19 23:34:51 -0500

    Move tests out of main.c

--- Final/src/calc/infixtopostfix.c
+++ Final/src/calc/infixtopostfix.c
@@ -67,11 +67,51 @@ int infixToPostfix(queue input, queue inputBits, queue * dataOut, queue * bitsOu
     return 0; //return 0 if done and not failed
 }
 
+//FUNCTION:
+
+int populateDummy(queue *data, queue *bin) 
+{
+    //populate the data
+    queue_enqueue(data, 5);
+    queue_enqueue(data, 1);
+    queue_enqueue(data, 53);
+    queue_enqueue(data, 2);
+    queue_enqueue(data, 2);
+    queue_enqueue(data, 1);
+    queue_enqueue(data, 34);
+    queue_enqueue(data, 2);
+    queue_enqueue(data, 19);
+
+    //populate the bin
+    queue_enqueue(bin, 0);
+    queue_enqueue(bin, 1);
+    queue_enqueue(bin, 0);
+    queue_enqueue(bin, 1);
+    queue_enqueue(bin, 0);
+    queue_enqueue(bin, 1);
+    queue_enqueue(bin, 0);
+    queue_enqueue(bin, 1);
+    queue_enqueue(bin, 0);
+}
+
 //Main Function For Testing! Uncomment When needed
 
-/*
+
 int main(void)
 {
+    queue dummyData = {-1, -1, 0};
+    queue dummyBin = {-1, -1, 0};
+    
+    populateDummy(&dummyData, &dummyBin);
+    
+    queue_printer_formatted(&dummyData);
+    queue_printer_formatted(&dummyBin);
 
+    queue goodData = {-1, -1, 0};
+    queue goodBin = {-1, -1, 0};
+
+    infixToPostfix(dummyData, dummyBin, &goodData, &goodBin);
+
+    queue_printer_formatted(&goodData);
+    queue_printer_formatted(&goodBin);
 } 
-*/
--- Final/src/main.c
+++ Final/src/main.c
@@ -2,54 +2,9 @@
 #include "main.h"
 
 
-//FUNCTION:
-
-int populateDummy(queue *data, queue *bin) 
-{
-    //populate the data
-    queue_enqueue(data, 5);
-    queue_enqueue(data, 1);
-    queue_enqueue(data, 53);
-    queue_enqueue(data, 2);
-    queue_enqueue(data, 2);
-    queue_enqueue(data, 1);
-    queue_enqueue(data, 34);
-    queue_enqueue(data, 2);
-    queue_enqueue(data, 19);
-
-    //populate the bin
-    queue_enqueue(bin, 0);
-    queue_enqueue(bin, 1);
-    queue_enqueue(bin, 0);
-    queue_enqueue(bin, 1);
-    queue_enqueue(bin, 0);
-    queue_enqueue(bin, 1);
-    queue_enqueue(bin, 0);
-    queue_enqueue(bin, 1);
-    queue_enqueue(bin, 0);
-}
-
 // Main Calculator Function
 int main (void)
 {
-    calcProfile * testProfile = initCalc(10);
-
-    /*
-    queue dummyData = {-1, -1, 0};
-    queue dummyBin = {-1, -1, 0};
-    
-    populateDummy(&dummyData, &dummyBin);
-    
-    queue_printer_formatted(&dummyData);
-    queue_printer_formatted(&dummyBin);
-
-    queue goodData = {-1, -1, 0};
-    queue goodBin = {-1, -1, 0};
-
-    infixToPostfix(dummyData, dummyBin, &goodData, &goodBin);
-
-    queue_printer_formatted(&goodData);
-    queue_printer_formatted(&goodBin);
-    */
+    //calcProfile * testProfile = initCalc(10);
 
 }
