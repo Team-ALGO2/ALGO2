a6dda91893b84a730e9989a6f2c48b858bda4fc1
SuperZooper3 <superzooper3@gmail.com>
2020-03-08 18:07:38 -0400

    Made calc.c for people who dont want to use the server

---
+++ Final/src/calc/calc.c
@@ -0,0 +1,36 @@
+#include "../main.h"
+
+//this is a funciton to run the calculator without the GUI
+int main(void)
+{
+    char str[MAX_INPUT_LENGTH];
+
+    printf("Please input an infix expression with no spaces or negative numbers:\n");
+    scanf("%s", &str);
+    printf("Expression inputed, computing:\n");
+
+    queue dummyData = {-1, -1, 0};
+    queue dummyBin = {-1, -1, 0};
+    
+    populate(&dummyData, &dummyBin,str);
+
+    dataQueueToString(dummyData, dummyBin);
+    
+    
+    //queue_printer_formatted(&dummyData);
+    //queue_printer_formatted(&dummyBin);
+
+    queue goodData = {-1, -1, 0};
+    queue goodBin = {-1, -1, 0};
+
+    infixToPostfix(dummyData, dummyBin, &goodData, &goodBin);
+
+    //queue_printer_formatted(&goodData);
+    //queue_printer_formatted(&goodBin);
+
+    dataQueueToString(goodData, goodBin);
+    printf("Evaluating:\n");
+    long double res = postfixCalc(goodData, goodBin);
+    printf("Result is: %Lf\n", res);
+    return 0;
+} 
